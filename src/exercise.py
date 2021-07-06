de806ee09e2d6f16bcbbdc70ee684f7ef6afc5c9
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-06 13:36:39 +0100

    Attempt 1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    first = int(input('Give the first number:'))
+    second = int(input('Give the second number:'))
 
+    average = (first + second) / 2
+
+    print('The avergae is ' + str(average))
 if __name__ == '__main__':
     main()
